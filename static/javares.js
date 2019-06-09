a65125131c8a9391387ed92586230b063e9b03f0
vialekis <vialekis@yandex.ru>
2019-06-09 12:05:30 +0600

    all

---
+++ static/javares.js
@@ -0,0 +1,26 @@
+﻿$("#main-header").click(function(){
+	$.get("/");
+    });
+    set_content_in_divs(paragraphs);
+    set_header();
+});
+
+function set_secret_message(msg) {
+    p = $("#second-paragraph");
+    p.html(msg)
+    p.css("color","red")
+}
+
+function set_content_in_divs(paragraphs) {
+    $.each(paragraphs, function(i, d) {
+      p = $("#p-" + i)
+      p.html("<p>" + d + "</p>")
+    })
+  }
+  
+function set_header() {
+    d = new Date();   
+    p = $("#main-header")
+    p.html("Что день " + d.getFullYear()+"-"+d.getMonth()+"-"+d.getDate()+" готовит")
+    /*p.html("Что день " +"dsdasd" + " готовит")*/
+  }
